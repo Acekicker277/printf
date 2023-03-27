6174f1fd2879004d7df10bcc7a64eb8952e250f5
Acekicker277 <oyetolaayo@gmail.com>
2023-03-27 16:49:29 -0700

    Printf

--- tests/main.c
+++ tests/main.c
@@ -1,6 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - Entry point
@@ -13,7 +13,7 @@ int main(void)
 	int len2;
 	unsigned int ui;
 	void *addr;
-
+	
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
@@ -39,6 +39,6 @@ int main(void)
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
-	printf("Unknown:[%r]\n");
+	//printf("Unknown:[%r]\n");
 	return (0);
 }
